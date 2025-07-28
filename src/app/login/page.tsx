2852b579aa9cb2e035eb5e8fee77f3da9f4a57c1
luofanlf <luofan036@gmail.com>
2025-07-28 19:24:45 +0800

    Fix: update login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -44,7 +44,7 @@ export default function LoginPage() {
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
         <a href="#" className="flex items-center gap-2 self-center font-medium">
-          <p className="text-2xl">FlashJudge</p>
+          <p className="text-2xl">HDBPilot</p>
         </a>
         <LoginForm onLogin={handleLogin} error={error} />
       </div>
